02bd95a43d55d511eb39d88c28a9beb304d89f47
clem9598 <47598609+clem9598@users.noreply.github.com>
2019-10-28 09:30:21 +0100

    Update EXO16.py

--- EXO16.py
+++ EXO16.py
@@ -1,9 +1,9 @@
-print("Entrer deux mots de la même longueure")
+print("Entrer deux mots de la même longueur")
 mot1 = input("Premier mot : ")
 mot2 = input("Deuxieme mot : ")
 nombre_lettre = len(mot1)
 
-#Fontion distance hamming
+# Fonction distance hamming
 def distance_hamming(var1,var2) :
     compteur = 0
     for x in range (nombre_lettre) :
@@ -11,7 +11,7 @@ def distance_hamming(var1,var2) :
             compteur += 1
     print (compteur)
 
-#On vérifie que les deux mots sont de la même longueur, si oui on execute la fonction
+# On vérifie que les deux mots sont de la même longueur, si oui on éxecute la fonction
 if len(mot1) == len(mot2) :
     distance_hamming(mot1,mot2)
 else :
