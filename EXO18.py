111ba1f90c20de65a7e5e4faa61e870f297f44fa
clem9598 <47598609+clem9598@users.noreply.github.com>
2019-10-28 16:54:37 +0100

    Add files via upload

---
+++ EXO18.py
@@ -0,0 +1,10 @@
+def recherche_suspect(ADN, nom, code_1, code_2) :
+    if (ADN.find(code_1) and ADN.find(code_2)!= -1) and (ADN.find(code_1) - ADN.find(code_2) > 4) :
+        return nom + " est coupable ! On retrouve ses brains d'ADN " + code_1 + " et " + code_2 + " dans sa séquence ADN."
+    else :
+        return nom + " n'est pas  coupable."
+        
+print(recherche_suspect("CCTGGAGGGTGGCCCCACCGGCCGAGACAGCGAGCATATGCAGGAAGCGGCAGGAATAAGGAAAAGCAGCADN","Moutarde","CATA","ATGC"))
+print(recherche_suspect("CTCCTGATGCTCCTCGCTTGGTGGTTTGAGTGGACCTCCCAGGCCAGTGCCGGGCCCCTCATAGGAGAGGADN","Rose","CATA","ATGC"))
+print(recherche_suspect("AAGCTCGGGAGGTGGCCAGGCGGCAGGAAGGCGCACCCCCCCAGTACTCCGCGCGCCGGGACAGAATGCCADN","Pervenche","CATA","ATGC")) 
+print(recherche_suspect("CTGCAGGAACTTCTTCTGGAAGTACTTCTCCTCCTGCAAATAAAACCTCACCCATGAATGCTCACGCAAG","Leblanc","CATA","ATGC"))
