cc74a1851c0114244bff069667ae25107cf988c7
clem9598 <47598609+clem9598@users.noreply.github.com>
2019-10-28 20:35:37 +0100

    Update EXO17.py

--- EXO17.py
+++ EXO17.py
@@ -1,11 +1,18 @@
-mot = input("Entrer un mot : ")
-
 # Fonction Palyndrome
 def palyndrome(var1) :
     if var1[::] == var1[::-1] :
         print  (mot, "est un palyndrome.")
     else :
         print (mot, "n'est pas un palyndrome.")
+        
+start = True
 
-# Execution de la fonction
-palyndrome(mot)
+#Boucle pour rejouer 
+while start : 
+  reponse = input("Voulez vous jouer (o/n) ?")
+  if reponse == "o" or reponse =="O" :
+    mot = input("Entrer un mot : ")
+    palyndrome(mot) # Execution de la fonction
+  else : 
+    start = False 
+
