3d1fda96d6e88ad4135a98f750fe39d9a75917ff
clem9598 <47598609+clem9598@users.noreply.github.com>
2019-10-31 10:11:48 +0100

    Update EXO18.py

--- EXO18.py
+++ EXO18.py
@@ -1,9 +1,11 @@
+# Fonction Recherche_suspect
 def recherche_suspect(ADN, nom, code_1, code_2) :
     if (ADN.find(code_1) and ADN.find(code_2)!= -1) and (ADN.find(code_1) - ADN.find(code_2) > 4) :
         return nom + " est coupable ! On retrouve ses brains d'ADN " + code_1 + " et " + code_2 + " dans sa séquence ADN."
     else :
         return nom + " n'est pas  coupable."
-        
+    
+# Execution de la fonction pour les 4 suspects, avec les paramètres ADN, NOM, CODE 1 et 2
 print(recherche_suspect("CCTGGAGGGTGGCCCCACCGGCCGAGACAGCGAGCATATGCAGGAAGCGGCAGGAATAAGGAAAAGCAGCADN","Moutarde","CATA","ATGC"))
 print(recherche_suspect("CTCCTGATGCTCCTCGCTTGGTGGTTTGAGTGGACCTCCCAGGCCAGTGCCGGGCCCCTCATAGGAGAGGADN","Rose","CATA","ATGC"))
 print(recherche_suspect("AAGCTCGGGAGGTGGCCAGGCGGCAGGAAGGCGCACCCCCCCAGTACTCCGCGCGCCGGGACAGAATGCCADN","Pervenche","CATA","ATGC")) 
